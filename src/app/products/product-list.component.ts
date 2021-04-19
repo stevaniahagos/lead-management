4e4769897f41863edb8e69d4b945b4b032d68c13
Stevania Hagos <stevaniahagos@gmail.com>
2021-04-19 13:35:43 +0800

    full CRUD using Angular & basic JS collection

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -11,16 +11,20 @@ export class ProductListComponent {
   nameOfLead = '';
   nameOfManager = 'Select Manager';
   code = 'Select Product';
+  leadIndex = 0;
+  isUpdate = false;
   products: any[] = [
     {
       productName: 'Health Insurance',
       productCode: 0,
       leads: [
         {
+          id: 0,
           leadName: 'Nessa Geron',
           managerName: 'Stevania Hagos',
         },
         {
+          id: 1,
           leadName: 'Allison Nestor',
           managerName: 'Stevania Hagos'
         }
@@ -33,10 +37,12 @@ export class ProductListComponent {
       productCode: 1,
       leads: [
         {
+          id: 0,
           leadName: 'Spencer Nelson',
           managerName: 'Inigo Montoya'
         },
         {
+          id: 1,
           leadName: 'Mark Torres',
           managerName: 'Stevania Hagos'
         }
@@ -50,7 +56,47 @@ export class ProductListComponent {
     this.products[productCode].showLeads = !this.products[productCode].showLeads;
   }
 
+  toggleIsUpdate(pCode: number, lIndex: number): void {
+    this.isUpdate = !this.isUpdate;
+    this.nameOfLead = this.products[pCode].leads[lIndex].leadName;
+    this.nameOfManager = this.products[pCode].leads[lIndex].managerName;
+    this.code = this.products[pCode].productCode;
+    this.leadIndex = lIndex;
+  }
+
   updateLeads(productCode: string, leadName: string, managerName: string): void {
     console.log(productCode + ' -- ' + leadName + ' -- ' + managerName);
+    const code = Number(productCode);
+    if (!this.isUpdate){
+      if (this.products[code].leads.length < 1) {
+        this.products[code].leads.push({
+          id: 0,
+          leadName: this.nameOfLead,
+          managerName: this.nameOfManager
+        });
+      } else {
+        this.products[code].leads.push({
+          id: this.products[code].leads[this.products[code].leads.length - 1].id + 1,
+          leadName: this.nameOfLead,
+          managerName: this.nameOfManager
+        });
+      }
+    } else{
+      console.log('update');
+      this.products[code].leads[this.leadIndex].leadName = this.nameOfLead;
+      this.products[code].leads[this.leadIndex].managerName = this.nameOfManager;
+      this.isUpdate = false;
+    }
+  }
+
+  deleteLead(productCode: string, leadId: string): void {
+    const code = Number(productCode);
+    if (confirm('Are you sure you want to delete this lead?')) {
+      for (let index = 0; index < this.products[code].leads.length; index++){
+        if (this.products[code].leads[index].id === Number(leadId)) {
+          this.products[code].leads.splice(index, 1);
+        }
+      }
+    }
   }
 }
